d2a458631c1f4ca1b946430ee9a372c2c4398f90
o140445 <o140445s@gmail.com>
2025-05-23 21:27:14 +0800

    add put on records

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,10 +40,13 @@
 
         <!-- 页脚 -->
         <footer class="bg-white border-t border-cyan-100">
-            <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-                <div class="text-center text-gray-500">
-                    <p>&copy; {{ date('Y') }} 游戏币. All rights reserved.</p>
-                </div>
+            <div class="text-center text-gray-500 py-4">
+                <p>&copy; {{ date('Y') }} 游戏币. All rights reserved.</p>
+                <p class="mt-2">
+                    <a href="https://beian.miit.gov.cn/" target="_blank" class="hover:text-cyan-600 transition duration-300">
+                    湘ICP备2022002846号-1
+                    </a>
+                </p>
             </div>
         </footer>
     </div>
